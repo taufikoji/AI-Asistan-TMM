12312dbcb6a3f9f499d66646b73addfc4b457d9c
taufikoji <113406120+taufikoji@users.noreply.github.com>
2025-06-13 23:39:34 +0700

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,4 @@
-# gunicorn.conf.py
-workers = 1  # Mulai dengan 1 worker
-timeout = 120  # Tingkatkan untuk proses lambat (OCR/API)
-bind = "0.0.0.0:${PORT}"  # Gunakan port dari Railway
+workers = 1
+timeout = 120
+bind = "0.0.0.0:${PORT}"
 loglevel = "debug"
