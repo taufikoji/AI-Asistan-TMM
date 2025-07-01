c3ee487d546cdd98096f6cd47ca3195c63ea10ab
taufikoji <113406120+taufikoji@users.noreply.github.com>
2025-07-02 00:47:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -122,10 +122,13 @@ def chat():
     if kategori == "brosur":
         brosur_url = request.host_url.rstrip("/") + "/download-brosur"
         reply = (
-            "Berikut adalah brosur resmi Trisakti School of Multimedia.<br><br>"
-            "Anda dapat mengunduhnya melalui tautan berikut: "
-            "<a href='https://chatbot-tmm-production.up.railway.app/download-brosur' target='_blank'>Download Brosur PDF</a><br><br>"
-            "Jika tidak dapat membuka, salin dan tempel link di browser Anda."
+            brosur_url = request.host_url.rstrip("/") + "/download-brosur"
+        reply = (
+    "Berikut adalah brosur resmi Trisakti School of Multimedia.<br><br>"
+    f"Anda dapat mengunduhnya melalui tautan berikut: "
+    f"<a href='{brosur_url}' target='_blank'>Download Brosur PDF</a><br><br>"
+    "Jika tidak dapat membuka, salin dan tempel link di browser Anda."
+)
         )
         save_chat(corrected, reply)
         return jsonify({
